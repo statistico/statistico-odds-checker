9d617dfc60e1a27d6ad4d9fd2e7cf772580159ca
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-10-30 12:33:23 +0000

    Run go fmt

--- internal/market/repository.go
+++ internal/market/repository.go
@@ -1,10 +1,10 @@
 package market
 
 type RepositoryQuery struct {
-	EventID     *uint64
-	MarketName  *string
-	Side        *string
-	SortBy      *string
+	EventID    *uint64
+	MarketName *string
+	Side       *string
+	SortBy     *string
 }
 
 type Repository interface {
