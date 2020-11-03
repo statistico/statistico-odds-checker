f97aa18f6d86df858e250d1c958d51038f5df869
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-11-03 15:58:09 +0000

    Add implemented football event market requester

--- internal/market/type.go
+++ internal/market/type.go
@@ -1,12 +1,8 @@
 package market
 
 import (
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
 	"github.com/statistico/statistico-odds-checker/internal/exchange"
 	"github.com/statistico/statistico-odds-checker/internal/grpc/proto"
-	"time"
 )
 
 type Market struct {
@@ -15,19 +11,5 @@ type Market struct {
 	Side           string          `json:"side"`
 	Exchange       string          `json:"exchange"`
 	ExchangeMarket exchange.Market `json:"exchange_market"`
-	StatisticoOdds []*proto.Odds    `json:"statistico_odds"`
-	Timestamp      time.Time       `json:"timestamp"`
-}
-
-func (m Market) Value() (driver.Value, error) {
-	return json.Marshal(m)
-}
-
-func (m *Market) Scan(value interface{}) error {
-	b, ok := value.([]byte)
-	if !ok {
-		return errors.New("type assertion to []byte failed")
-	}
-
-	return json.Unmarshal(b, &m)
+	StatisticoOdds []*proto.Odds   `json:"statistico_odds"`
 }
---
+++ internal/sport/football.go
@@ -0,0 +1,90 @@
+package sport
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/jonboulle/clockwork"
+	"github.com/sirupsen/logrus"
+	"github.com/statistico/statistico-odds-checker/internal/grpc"
+	"github.com/statistico/statistico-odds-checker/internal/grpc/proto"
+	"github.com/statistico/statistico-odds-checker/internal/market"
+	"time"
+)
+
+const football = "football"
+
+type footballEventMarketRequester struct {
+	fixtureClient    grpc.FixtureClient
+	builder          market.Builder
+	logger           *logrus.Logger
+	clock            clockwork.Clock
+	seasons          []uint64
+	markets          []string
+}
+
+func (f *footballEventMarketRequester) FindEventMarkets(ctx context.Context, from, to time.Time) <-chan *EventMarket {
+	req := proto.FixtureSearchRequest{
+		SeasonIds:  f.seasons,
+		DateBefore: &wrappers.StringValue{Value: to.Format(time.RFC3339)},
+		DateAfter:  &wrappers.StringValue{Value: from.Format(time.RFC3339)},
+	}
+
+	fixtures, err := f.fixtureClient.Search(ctx, &req)
+
+	if err != nil {
+		f.logger.Errorf("Error %q fetching fixtures in football market requester", err.Error())
+		return nil
+	}
+
+	ch := make(chan *EventMarket, len(fixtures))
+
+	go f.buildEventMarkets(ctx, fixtures, ch)
+
+	return ch
+}
+
+func (f *footballEventMarketRequester) buildEventMarkets(ctx context.Context, fixtures []*proto.Fixture, ch chan<- *EventMarket) {
+	for _, fx := range fixtures {
+		date := time.Unix(fx.DateTime.Utc, 0)
+
+		q := market.BuilderQuery{
+			Date:    date,
+			Event:   fmt.Sprintf("%s v %s", fx.HomeTeam.Name, fx.AwayTeam.Name),
+			EventID: uint64(fx.Id),
+			Sport:   football,
+			Markets: f.markets,
+		}
+
+		for m := range f.builder.Build(ctx, &q) {
+			ch <- convertToEventMarket(m, fx.DateTime.Utc, f.clock.Now())
+		}
+	}
+
+	close(ch)
+}
+
+func convertToEventMarket(m *market.Market, date int64, timestamp time.Time) *EventMarket {
+	return &EventMarket{
+		EventID:        m.EventID,
+		Sport:          football,
+		EventDate:      date,
+		MarketName:     m.Name,
+		Side:           m.Side,
+		Exchange:       m.Exchange,
+		ExchangeMarket: m.ExchangeMarket,
+		StatisticoOdds: m.StatisticoOdds,
+		Timestamp:      timestamp.Unix(),
+	}
+}
+
+func NewFootballEventMarketRequester(f grpc.FixtureClient, b market.Builder, l *logrus.Logger, c clockwork.Clock, s []uint64, m []string) EventMarketRequester {
+	return &footballEventMarketRequester{
+		fixtureClient: f,
+		builder:       b,
+		logger:        l,
+		clock:         c,
+		seasons:       s,
+		markets:       m,
+	}
+}
