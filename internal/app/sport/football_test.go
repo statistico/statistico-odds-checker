0935a9dfa76f608a167894a25f11cb8864b4bd65
Joe Sweeny <joesweeny84@hotmail.com>
2021-04-16 10:40:16 +0100

    Update fixture time difference to two hours

--- internal/app/sport/football_test.go
+++ internal/app/sport/football_test.go
@@ -119,7 +119,7 @@ func TestFootballEventMarketRequester_FindEventMarkets(t *testing.T) {
 		a.Equal(int64(1547465100), emTwo.Timestamp)
 	})
 
-	t.Run("builder is not called if fixture date and current date difference is greater than three hours", func(t *testing.T) {
+	t.Run("builder is not called if fixture date and current date difference is greater than two hours", func(t *testing.T) {
 		t.Helper()
 
 		fixClient := new(MockFixtureClient)
