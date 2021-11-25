b8952a19e0a5c9540eeb7d518a269a60d753559d
Joe Sweeny <joesweeny84@hotmail.com>
2021-11-25 20:01:33 +0000

    Update release images script to latest tag

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
-docker tag "statisticooddschecker_console" "$AWS_ECR_ACCOUNT_URL/statistico-odds-checker:$CIRCLE_SHA1"
-docker push "$AWS_ECR_ACCOUNT_URL/statistico-odds-checker:$CIRCLE_SHA1"
+docker tag "statisticooddschecker_console" "$AWS_ECR_ACCOUNT_URL/statistico-odds-checker:latest"
+docker push "$AWS_ECR_ACCOUNT_URL/statistico-odds-checker:latest"
