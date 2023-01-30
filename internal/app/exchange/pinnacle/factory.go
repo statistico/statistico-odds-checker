f489808dc007ee9dff7306e594e73b60f5da9037
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-01-30 14:12:01 +0000

    Add pinnacle market factory

--- internal/app/exchange/pinnacle/factory.go
+++ internal/app/exchange/pinnacle/factory.go
@@ -1,1 +1,39 @@
 package pinnacle
+
+import (
+	"context"
+	"fmt"
+	"github.com/statistico/statistico-odds-checker/internal/app/exchange"
+	"github.com/statistico/statistico-odds-checker/internal/app/sportmonks"
+)
+
+var exchangeID = 70
+
+type marketFactory struct {
+	parser sportmonks.OddsParser
+}
+
+func (m *marketFactory) CreateMarket(ctx context.Context, e *exchange.Event) (*exchange.Market, error) {
+	odds, err := m.parser.ParseMarketOdds(ctx, int(e.ID), exchangeID, e.Market)
+
+	if err != nil {
+		return nil, err
+	}
+
+	runners, err := ConvertOddsToRunners(odds)
+
+	if err != nil {
+		return nil, fmt.Errorf("error converting odds to runners in Pinnacle exchange: %s", err.Error())
+	}
+
+	return &exchange.Market{
+		ID:           "NONE",
+		Name:         e.Market,
+		ExchangeName: "PINNACLE",
+		Runners:      runners,
+	}, nil
+}
+
+func NewMarketFactory(p sportmonks.OddsParser) exchange.MarketFactory {
+	return &marketFactory{parser: p}
+}
