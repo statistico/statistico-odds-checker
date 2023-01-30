a9ee2df4c168f904e98f61bf30d81853474c57b9
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-01-30 14:39:51 +0000

    Add tests of converting odds to runners

--- internal/app/exchange/utility.go
+++ internal/app/exchange/utility.go
@@ -7,13 +7,13 @@ import (
 )
 
 func ConvertOddsToRunners(odds []sportmonks.Odds) ([]*Runner, error) {
-	runners := []*Runner{}
+	var runners []*Runner
 
 	for _, o := range odds {
 		price, err := strconv.ParseFloat(o.Dp3, 32)
 
 		if err != nil {
-			return nil, fmt.Errorf("value '%s' is not a valid decimal", o.Dp3)
+			return nil, fmt.Errorf("value '%s' is not a valid floating point number", o.Dp3)
 		}
 
 		runners = append(runners, &Runner{
---
+++ internal/app/exchange/utility_test.go
@@ -0,0 +1,79 @@
+package exchange_test
+
+import (
+	"github.com/statistico/statistico-odds-checker/internal/app/exchange"
+	"github.com/statistico/statistico-sportmonks-go-client"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConvertOddsToRunners(t *testing.T) {
+	t.Run("converts sportsmonks.Odds slice to Runner slice", func(t *testing.T) {
+		t.Helper()
+
+		odds := []sportmonks.Odds{
+			{
+				Label: "Over",
+				Dp3:   "1.98",
+			},
+			{
+				Label: "Under",
+				Dp3:   "2.55",
+			},
+		}
+
+		runners, err := exchange.ConvertOddsToRunners(odds)
+
+		if err != nil {
+			t.Fatalf("Expected nil, got %s", err.Error())
+		}
+
+		expected := []*exchange.Runner{
+			{
+				ID:   0,
+				Name: "Over",
+				BackPrices: []exchange.PriceSize{
+					{
+						Price: 1.98,
+						Size:  0,
+					},
+				},
+			},
+			{
+				ID:   0,
+				Name: "Under",
+				BackPrices: []exchange.PriceSize{
+					{
+						Price: 2.55,
+						Size:  0,
+					},
+				},
+			},
+		}
+
+		assert.Equal(t, expected, runners)
+	})
+
+	t.Run("return an error if price value is not a valid float", func(t *testing.T) {
+		t.Helper()
+
+		odds := []sportmonks.Odds{
+			{
+				Label: "Over",
+				Dp3:   "1.98",
+			},
+			{
+				Label: "Under",
+				Dp3:   "HeeeHeee",
+			},
+		}
+
+		_, err := exchange.ConvertOddsToRunners(odds)
+
+		if err == nil {
+			t.Fatal("Expected error, got nil")
+		}
+
+		assert.Equal(t, "value 'HeeeHeee' is not a valid floating point number", err.Error())
+	})
+}
