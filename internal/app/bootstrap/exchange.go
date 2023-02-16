2615a5aa7f7839ce55c663d823da60703ae007d6
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-02-16 09:53:59 +0000

    Add bet365 and betcris market factory implementations

--- internal/app/bootstrap/exchange.go
+++ internal/app/bootstrap/exchange.go
@@ -3,6 +3,8 @@ package bootstrap
 import (
 	bfc "github.com/statistico/statistico-betfair-go-client"
 	"github.com/statistico/statistico-odds-checker/internal/app/exchange"
+	"github.com/statistico/statistico-odds-checker/internal/app/exchange/bet365"
+	"github.com/statistico/statistico-odds-checker/internal/app/exchange/betcris"
 	"github.com/statistico/statistico-odds-checker/internal/app/exchange/betfair"
 	"github.com/statistico/statistico-odds-checker/internal/app/exchange/pinnacle"
 	"net/http"
@@ -31,12 +33,22 @@ func (c Container) BetfairMarketFactory() exchange.MarketFactory {
 	return betfair.NewMarketFactory(client)
 }
 
+func (c Container) Bet365MarketFactory() exchange.MarketFactory {
+	return bet365.NewMarketFactory(c.SportmonksOddsParser())
+}
+
+func (c Container) BetCrisMarketFactory() exchange.MarketFactory {
+	return betcris.NewMarketFactory(c.SportmonksOddsParser())
+}
+
 func (c Container) PinnacleMarketFactory() exchange.MarketFactory {
 	return pinnacle.NewMarketFactory(c.SportmonksOddsParser())
 }
 
 func (c Container) MarketBuilder() exchange.MarketBuilder {
 	factories := []exchange.MarketFactory{
+		c.Bet365MarketFactory(),
+		c.BetCrisMarketFactory(),
 		c.PinnacleMarketFactory(),
 	}
 
