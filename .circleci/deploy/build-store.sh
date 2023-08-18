f9b1cd88cce9e0842226786b30873064ee421e0c
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-08-18 16:14:46 +0100

    Update deploy and build scripts

--- .circleci/deploy/build-store.sh
+++ .circleci/deploy/build-store.sh
@@ -4,8 +4,6 @@ set -e
 
 mkdir -p /tmp/workspace/artifacts
 
-CGO_ENABLED=0 GOOS=linux go build -a -o /tmp/workspace/artifacts/statistico-odds-checker ./lambda/main.go
+CGO_ENABLED=0 GOOS=linux go build -a -o statistico-odds-checker ./lambda/main.go
 
-cd /tmp/workspace/artifacts
-
-zip statistico-odds-checker.zip statistico-odds-checker
+zip statistico-odds-checker.zip /tmp/workspace/artifacts/statistico-odds-checker
