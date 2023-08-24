576f049a6be504f200ce94018717b1ae93c70383
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-08-24 12:28:24 +0100

    Remove legacy deploy scripts

--- .circleci/deploy/docker-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
-
-docker tag "statistico-odds-checker-console" "$AWS_ECR_ACCOUNT_URL/statistico-odds-checker:latest"
-docker push "$AWS_ECR_ACCOUNT_URL/statistico-odds-checker:latest"
--- .circleci/deploy/docker-save.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mkdir -p /tmp/workspace/docker-cache
-
-docker save -o /tmp/workspace/docker-cache/statisticooddschecker_console.tar statistico-odds-checker-console:latest
