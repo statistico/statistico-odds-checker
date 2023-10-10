1c2d5acabf00b741b60d8a70cf382b1e6e096ae6
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-10-10 14:49:08 +0100

    Add fetching statistico support for over and under markets

--- internal/app/exchange/statistico/utility.go
+++ internal/app/exchange/statistico/utility.go
@@ -1,7 +1,7 @@
 package statistico
 
 func marketIsSupported(market string) bool {
-	markets := []string{OverUnder25}
+	markets := []string{OverUnder05, OverUnder15, OverUnder25, OverUnder35}
 
 	for _, m := range markets {
 		if m == market {
