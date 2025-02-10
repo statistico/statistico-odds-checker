3ed21adaed14d70b99031a22d3eb3ae1441d58be
Joe Sweeny <joesweeny84@hotmail.com>
2025-02-10 20:58:10 +0000

    Update environment variables and remove unused logic

--- internal/app/bootstrap/data.go
+++ internal/app/bootstrap/data.go
@@ -2,6 +2,6 @@ package bootstrap
 
 import "github.com/statistico/statistico-football-data-go-grpc-client"
 
-func (c Container) DataServiceResultClient() statisticodata.FixtureClient {
-	return statisticodata.NewFixtureClient(c.GrpcFixtureClient())
+func (c Container) DataServiceResultClient() statisticofootballdata.FixtureClient {
+	return statisticofootballdata.NewFixtureClient(c.GrpcFixtureClient())
 }
